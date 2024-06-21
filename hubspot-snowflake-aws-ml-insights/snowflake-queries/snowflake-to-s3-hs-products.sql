7e485a7ba1f4733406cd01e10eb2913891323533
robertainslie <robert.p.ainslie@gmail.com>
2024-06-21 11:25:54 -0400

    added note about join

--- hubspot-snowflake-aws-ml-insights/snowflake-queries/snowflake-to-s3-hs-products.sql
+++ hubspot-snowflake-aws-ml-insights/snowflake-queries/snowflake-to-s3-hs-products.sql
@@ -6,6 +6,8 @@ Notes to customize this query:
 2. Update the S3 Bucket URL based on the bucket created by the AWS Personalize ML OpsStack CloudFormation template
 3. Follow the documentation from Snowflake on unloading data from Snowflake to S3: https://docs.snowflake.com/en/user-guide/data-unload-s3
 Note: This query uses an AWS Secret Key tied to an AWS IAM User to authorize the unload into S3, not a Snowflake stage with a dedicated IAM user, which is Snowflake's recommended approach
+
+Note on join: The query uses a LEFT OUTER JOIN to retrieve all product user interactions and the relevant product information for those interactions.
 */
 COPY INTO 's3://your-s3-bucket/train/items.csv'
 FROM (
